18172b5ee8d599d9f76c338efa8438fc7a30a381
David <davidarrozaqi085@gmail.com>
2025-09-16 08:22:36 +0700

    feat: add profile page

--- lib/app/modules/profile/customer_profile_controller.dart
+++ lib/app/modules/profile/customer_profile_controller.dart
@@ -1,3 +1,16 @@
 import 'package:get/get.dart';
 
-class CustomerProfileController extends GetxController{}
+class CustomerProfileController extends GetxController{
+  RxBool faceIdEnabled = false.obs;
+  // Dummy user data
+  final RxString name = 'Itunoluwa Abidaye'.obs;
+  final RxString username = '@itunoluwa'.obs;
+  final RxString avatarUrl = ''.obs;
+
+  void toggleFaceId(bool value) {
+    faceIdEnabled.value = value;
+  }
+
+  // Add more profile logic here as needed
+
+}
