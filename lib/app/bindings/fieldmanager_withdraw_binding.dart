6795371232a917aea1f17aa0c44df6d9774080e1
IvanoWoii <rezaivano123@gmail.com>
2025-09-23 14:20:09 +0700

    fix get controlet to get.put to getView

--- lib/app/bindings/fieldmanager_withdraw_binding.dart
+++ lib/app/bindings/fieldmanager_withdraw_binding.dart
@@ -2,6 +2,8 @@ import 'package:get/get.dart';
 
 import '../modules/fieldmanager_withdraw/fieldmanager_withdraw_controller.dart';
 
+import '../modules/fieldmanager_withdraw/fieldmanager_withdraw_controller.dart';
+
 class FieldmanagerWithdrawBinding extends Bindings {
   @override
   void dependencies() {
